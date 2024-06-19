b304bc12f5aaa1215d63b8547f56bb0077f6f163
mrpandacodes <omersaka@yahoo.com.tr>
2024-06-19 07:56:04 +0300

    implement book read status and book delete function

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const submitButton = document.getElementById("submit");
 const exitButton = document.getElementById("exit");
 const clearButton = document.getElementById("reset");
 
-let index = 0;
+let id = 0;
 
 
 
@@ -26,31 +26,50 @@ function displayBook() {
     let bookPaScWrapper = document.createElement("div");
     let bookBody = document.createElement("div");
     let buttonDeleteBook = document.createElement("button");
+    let buttonReadBook = document.createElement("button");
+    let wrapperButtons= document.createElement("div");
     let wrapper = document.createElement("div");
 
     bookTitle.textContent = last.name;
     bookBody.textContent = last.text;
     bookScore.textContent = `Score ${last.star} / 5`;
     bookPage.textContent = `Pages: ${last.pages}`;
-    buttonDeleteBook.setAttribute("data", `${last.index}`);
+    buttonDeleteBook.setAttribute("data", `${last.id}`);
+    buttonReadBook.setAttribute("data", `${last.id}`);
+    //Style created elements
+    wrapperButtons.classList.add("wrapperButtons");
+    buttonDeleteBook.classList.add("buttonDeleteBook");
+    buttonReadBook.classList.add("buttonReadBook");
     //Add created elements to DOM 
     book.appendChild(bookTitle);
     book.appendChild(bookPaScWrapper);
     bookPaScWrapper.appendChild(bookScore);
     bookPaScWrapper.appendChild(bookPage);
     book.appendChild(bookBody);
-    book.appendChild(buttonDeleteBook);
+    book.appendChild(wrapperButtons);
+    wrapperButtons.appendChild(buttonReadBook);
+    wrapperButtons.appendChild(buttonDeleteBook);
     wrapper.appendChild(book);
     display.appendChild(wrapper);
     //Add book delete buttons to an array
-    buttonArray=document.querySelectorAll("button[data]");
-    buttonArray.forEach(e => {
-        e.addEventListener("click", (el) => {
-            console.log(el);
-           let book = library[library.findIndex((x)=> x.index==el.target.getAttribute("data"))]
-           console.log(book);
-        })
-    
+    //Add eventlistener to book deleting button
+    buttonDeleteBook.addEventListener("click", (e)=>{
+        let book = library[library.findIndex((el)=> el.id==e.target.getAttribute("data"))];
+        book.remove();
+        let bookDisplay = e.target.parentNode.parentNode.parentNode;
+        bookDisplay.style.display="none";
+    })
+    buttonReadBook.addEventListener("click", (e)=>{
+        let book = library[library.findIndex((el)=> el.id==e.target.getAttribute("data"))];
+        console.log(book, book.read);
+        if(book.read==false){
+            book.read=true;
+            buttonReadBook.classList.add("check");
+        }
+        else{
+            book.read=false;
+            buttonReadBook.classList.remove("check");
+        }
     });
 }
 
@@ -60,14 +79,15 @@ class Book {
         this.text = bodyInputValue;
         this.star = star;
         this.pages = pages;
-        this.index = ++index;
-        console.log(this.index);
-        index = this.index;
+        this.id = ++id;
+        this.read = false;
+        console.log(this.id);
+        id = this.id;
         library.push(this);
-        console.log("book added success");
     }
-    remove(index) {
-        library.splice(library.findIndex((e) => e.index == index), 1);
+    //Find index of book object in library array using book.id and remove book object
+    remove() {
+        library.splice(library.findIndex((x) => x.id == this.id), 1);
     }
 }
 
@@ -103,4 +123,5 @@ clearButton.addEventListener("click", (e) => {
     console.log("cleared");
 });
 exitButton.addEventListener("click", (e) => { board.style.display = "none" });
+
 
